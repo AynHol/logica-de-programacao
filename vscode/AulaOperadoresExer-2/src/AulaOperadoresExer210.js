8c93c33d6f1e9a88f61d7430c8b0dced34600fcb
AynHol <wesley_a_antunes@estudante.sesisenai.org.br>
2024-06-18 19:52:13 -0300

    210

--- vscode/AulaOperadoresExer-2/src/AulaOperadoresExer210.js
+++ vscode/AulaOperadoresExer-2/src/AulaOperadoresExer210.js
@@ -1,3 +1,21 @@
 import leia from 'readline-sync'
 
-console.log("Quanto você pretende doar?")
+console.log("Quanto você pretende doar?");
+console.log("1 - 10R$");
+console.log("2 - 25R$");
+console.log("3 - 50R$");
+console.log("4 - Outro valor");
+var opcao = leia.question("Informe a opção: ");
+
+if(opcao == "1"){
+    console.log("Você doou 10R$, muito obrigado!");
+} else if(opcao == "2"){
+    console.log("Você doou 25R$, muito obrigado!");
+} else if(opcao == "3"){
+    console.log("Você doou 50R$, muito obrigado!");
+} else if (opcao == "4"){
+    var valor = leia.question("Informe o valor: ");
+    console.log("Você doou " + valor + "R$, muito obrigado!");
+} else{
+    console.log("Opção desconhecida.");
+}
