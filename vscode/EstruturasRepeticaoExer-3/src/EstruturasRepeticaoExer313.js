e41e36b4d5a0c89ce1a0b4922b2f971791cf81dc
AynHol <wesley_a_antunes@estudante.sesisenai.org.br>
2024-06-27 13:44:21 -0300

    313

--- vscode/EstruturasRepeticaoExer-3/src/EstruturasRepeticaoExer313.js
+++ vscode/EstruturasRepeticaoExer-3/src/EstruturasRepeticaoExer313.js
@@ -1,30 +1,13 @@
 import leia from 'readline-sync';
 
 var numero = leia.questionInt("Digite um numero decimal: ");
-console.log("O número decimal em binário é: ");
-var valor = numero;
 var bi = "";
 
-while(valor < 0){
-    var resul = parseInt(valor / 2);
-    var resul2 = valor % 2;
-    bi = bi + resul2;
-    valor = resul;
+while(numero > 0){
+    var resul = parseInt(numero / 2);
+    var resul2 = numero % 2;
+    bi = resul2 + bi;
+    numero = resul;
 }
 
-console.log()
-
-
-// numero / 2 == resul;
-// numero % 2 == resul2;
-
-// do {
-//     resul / 2 == resul;
-//     resul % 2 == resul2;
-
-//     if (resul == 1 || 0) {
-//         console.log(resul)
-//     }
-
-//     console.log(resul2)
-// } while (resul !== 1 || 0)
+console.log(bi)
