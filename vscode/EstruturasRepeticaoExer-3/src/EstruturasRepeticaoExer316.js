9d577958b15b711208708c3eadfd0a6a1d1c4f1b
AynHol <wesley_a_antunes@estudante.sesisenai.org.br>
2024-06-27 20:56:16 -0300

    extra 316

--- vscode/EstruturasRepeticaoExer-3/src/EstruturasRepeticaoExer316.js
+++ vscode/EstruturasRepeticaoExer-3/src/EstruturasRepeticaoExer316.js
@@ -14,6 +14,7 @@ if (modo === 1) {
 }
 
 var final = false;
+var tentavias= 0;
 
 if (modo === 1) {
     if (dificuldade === 1) {
@@ -22,12 +23,14 @@ if (modo === 1) {
             var adivinha = leia.questionInt("Digite o numero: ");
             if (adivinha === numero) {
                 console.log("Parabens você ganhou!!");
+                console.log("Com " + tentavias + " tentativas!");
                 var final = true;
             } else if (adivinha < numero) {
                 console.log("O número é maior");
             } else {
                 console.log("O número é menor");
             }
+            tentavias++;
         } while (final === false)
     } else if (dificuldade === 2) {
         var numero = Math.floor(Math.random() * 10001);
@@ -35,12 +38,14 @@ if (modo === 1) {
             var adivinha = leia.questionInt("Digite o numero: ");
             if (adivinha === numero) {
                 console.log("Parabens você ganhou!!");
+                console.log("Com " + tentavias + " tentativas!");
                 final = true;
             } else if (adivinha < numero) {
                 console.log("O número é maior");
             } else {
                 console.log("O número é menor");
             }
+            tentavias++;
         } while (final === false)
     } else {
         var numero = Math.floor(Math.random() * 1000001);
@@ -48,28 +53,32 @@ if (modo === 1) {
             var adivinha = leia.questionInt("Digite o numero: ");
             if (adivinha === numero) {
                 console.log("Parabens você ganhou!!");
+                console.log("Com " + tentavias + " tentativas!");
                 final = true;
             } else if (adivinha < numero) {
                 console.log("O número é maior");
             } else {
                 console.log("O número é menor");
             }
+            tentavias++;
         } while (final === false)
     }
 }
 
 else if (modo === 2) {
-    var numero = leia.questionInt("Digite um numero para o segundo jogador: ")
+    var numero = leia.questionInt("Digite um numero para o segundo jogador: ", {hideEchoBack:true})
     do {
         var adivinha = leia.questionInt("Digite o numero: ");
         if (adivinha === numero) {
             console.log("Parabens você ganhou!!");
+            console.log("Com " + tentavias + " tentativas!");
             final = true;
         } else if (adivinha < numero) {
             console.log("O número é maior");
         } else {
             console.log("O número é menor");
         }
+        tentavias++;
     } while (final === false)
 }
 
