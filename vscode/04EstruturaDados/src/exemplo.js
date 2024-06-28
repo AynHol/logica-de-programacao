4902816061467f2c4981412e64ac8cd9732ff9b7
AynHol <wesley_a_antunes@estudante.sesisenai.org.br>
2024-06-27 21:53:24 -0300

    03 Estrutura de dados

---
+++ vscode/04EstruturaDados/src/exemplo.js
@@ -0,0 +1,17 @@
+var lista = ["Wesley", "Alessandro"]
+
+console.log(lista[1])
+lista[1] = "Daré"
+
+console.log(lista)
+console.log(lista[1])
+console.log(lista.length)
+
+lista.forEach(item => {
+    //codigos
+})
+
+for (var i = 0; i < lista.length; i++) {
+    console.log(lista[i])
+    //codigos
+}
